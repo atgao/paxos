3caefdf650c6653100bcdc65ede71d611e5e55ae
Alice Gao <atgao@cs.washington.edu>
2021-11-20 16:05:03 -0800

    can send promise now...

--- paxos.go
+++ paxos.go
@@ -31,15 +31,21 @@ type Paxos struct {
 	// proposalId is also the highest one seen so far
 	proposalId 		 int  // NOTE THAT THIS SHOULD ALWAYS BE INCREASING
 	proposalAccepted bool // already accepted proposal ?? 
+	acceptedVal 	 int  // ?? 
 
 	// channels to always read from 
-	ch 				chan Message // channel to communicate from node --> network
+	ch 				chan Message // channel to communicate from node --> network ????
 	acceptor 		chan Message // channel to read from as acceptor 
 	learner  		chan Message // channel as learner
+	proposer 		chan Message // channel as proposer/leader
 
 
 	state 			string // leader (proposer) or acceptor
 	Log 			[]LogEntry // TODO: fix this... 
+}
+
+func quorum(n int) int{
+	return n/2 +1
 }
 
 // function for learner
@@ -49,6 +55,36 @@ func (px *Paxos) runLearner() {
 
 // function for acceptor
 func (px *Paxos) runAcceptor(){
+	
+	for {
+		msg := <- px.acceptor
+
+		switch msg.Type {
+		case "prepare":
+			if msg.ProposalId > px.proposalId {
+				px.proposalId = msg.ProposalId // update proposal 
+
+				// TODO: update proposal accepted boolean?? 
+				// TODO: check if the fields are right too...
+				promiseMessage := Message {
+					Type: "promise", 
+					ProposalId: msg.ProposalId, 
+					AcceptId: px.proposalId, 
+					Val: px.acceptedVal, 
+				}
+
+				px.net.recvQueue <- promiseMessage
+			}
+
+
+		// case "accept": 
+		// 	if msg.ProposalId >= px.ProposalId {
+		// 		// TODO: some stuff 
+		// 	}
+
+
+		}
+	}
 
 }
 
@@ -67,7 +103,26 @@ func (px *Paxos) Prepare() {
 
 }
 
-//
+func (px *Paxos) runProposer() {
+	// var npromised, q int = 0, quorum(px.net.nodes)
+	// for {
+	// 	msg := <- px.proposer 
+	// 	switch msg.Type {
+	// 	// case "propose": 
+	// 	// 	proposeMsg := {
+
+	// 	// 	}
+	// 	case "promise":
+	// 		if msg.ProposalId > px.proposalId
+
+
+	// 	}
+	// }
+
+}
+
+
+// TODO: need to figure this out...
 // kills this paxos node
 //
 func (px *Paxos) kill() {
@@ -79,14 +134,19 @@ func (px *Paxos) kill() {
 // 
 func Make(me int, net *Network) *Paxos {
 	px := &Paxos{}
-	px.me = me 
+	px.me = me // my index in the sendQueue array 
 	px.net = net
-	px.ch = px.net.sendQueue[me] // SHOULD ALWAYS BE THIS INDEX... 
+	px.ch = px.net.sendQueue[me] 
 
 	// init the paxos node 
-	px.state = "A" // for acceptor 
+	px.state = "A" // for acceptor ??? may need to fix...
 	px.Log = make([]LogEntry, 0) // TODO: is this needed...
 	px.proposalId = -1 // this way when we start its 0
+	px.proposalAccepted = false 
+	px.acceptedVal = -1 
+	px.acceptor = make(chan Message)
+	px.learner = make(chan Message)
+	px.proposer = make(chan Message)
 
 
 	go px.run()
@@ -97,7 +157,8 @@ func Make(me int, net *Network) *Paxos {
 // this go routine keeps on running in the background
 func (px *Paxos) run() {
 
-	// go px.runAcceptor()
+	go px.runProposer()
+	go px.runAcceptor()
 	// go px.runLearner()
 
 	// loop to listen to messages and 
@@ -108,6 +169,23 @@ func (px *Paxos) run() {
 			fmt.Printf("from %v to %v, %v\n", msg.From, px.me, msg)
 
 		// }
+
+		switch msg.Type {
+		case "prepare": // proposer --> acceptor 
+			px.acceptor <- msg 
+		case "accept": // proposer --> acceptor 
+			px.acceptor <- msg 
+		
+		// // do some stuff here for proposer
+		case "promise": 
+			px.proposer <- msg
+		// case "propose": I DONT THINK THIS ACTUALLY GOES HERE
+		// 	px.proposer <- msg 
+
+		case "accepted":
+			px.learner <- msg			
+		}
+		
 	}
 
 }
