eece78f60966129f5b72f141058e82ededa0ff8a
kaiming-uw <70674915+kaiming-uw@users.noreply.github.com>
2021-12-11 17:01:02 -0800

    fix

--- paxos.go
+++ paxos.go
@@ -151,8 +151,14 @@ func (state *GlobalState) ProcessSuccessMessage(msg SuccessMessage) SuccessRespo
 	return SuccessResponseMessage{FirstUnchosenIndex: state.PaxosNodeState.AcceptorPersistentState.FirstUnchosenIndex}
 }
 
+func(state *GlobalState) Broadcast(msg Message){
+	BroadcastPaxosMessage(state.InterNodeUDPSock, state.Config.AllPeerAddresses(), msg)
+}
+
+
 func (state *GlobalState) ProposerAlgorithm(inputValue ProposalValue) bool {
 	var index int
+	var n int
 	var value ProposalValue
 	var NumberOfNoMoreAccepted int
 
@@ -161,43 +167,69 @@ func (state *GlobalState) ProposerAlgorithm(inputValue ProposalValue) bool {
 	}
 	 
 	if state.PaxosNodeState.ProposerVolatileState.Prepared == true{
-		index = state.PaxosNodeState.ProposerVolatileState.NextIndex
+		index := state.PaxosNodeState.ProposerVolatileState.NextIndex
 		state.PaxosNodeState.ProposerVolatileState.NextIndex += 1
 	}else{
-		index = state.PaxosNodeState.AcceptorPersistentState.FirstUnchosenIndex
+		index := state.PaxosNodeState.AcceptorPersistentState.FirstUnchosenIndex
 		state.PaxosNodeState.ProposerVolatileState.NextIndex = (index+1)
-		//placeholder for broadcasting
-		BroadcastPaxosMessage()
-	}
-	//placeholder for receiving responses
-	NewPaxosMessage := <-
-	//placeholder
-	if majorityReached(){
-		NumberOfNoMoreAccepted = 0
-		Majorityqueue = []Message
-		maxAcceptedProposal := Majorityqueue[0]
+		n := state.PaxosNodeState.ProposerPersistentState.MaxRound + 1
+		
+		pn := ProposalNumber{N:n,SenderId: state.Config.SelfId}
 
-		for i:=0;i<len(Majorityqueue);i++{
-			if Majorityqueue[i]>max{
-				max = Majorityqueue[i]
-			}
-			if //placeholder
-			 	{
-				NumberOfNoMoreAccepted += 1
+		PrepareMsg := PrepareMessage{ProposalNumber:&pn,LogEntryIndex:&index}
+
+		Msg := Message{SenderID: state.Config.SelfId, Prepare: &PrepareMsg}
+		
+
+		dispatcher := MakeDispatcher(func(msg Message)bool{
+			if msg.PrepareResponse != nil{
+				if msg.PrepareResponse.AcceptedProposalNumber == pn && msg.PrepareResponse.LogEntryIndex == index{
+					return true
 				}
+				return false
+			}
+			return false
+		})
+		AddPaxosMessageDispatcher(state,dispatcher)
+
+		state.Broadcast(Msg)
+
+		defer RemovePaxosMessageDispatcher(state,dispatcher)
+		
+		for {
+			NewPaxosMessage := <-dispatcher.ch
+
+			if (majorityReached()){
+				NumberOfNoMoreAccepted = 0
+				Majorityqueue = []Message
+				maxAcceptedProposal := Majorityqueue[0]
+		
+				for i:=0;i<len(Majorityqueue);i++{
+					if Majorityqueue[i]>max{
+						max = Majorityqueue[i]
+					}
+					if //placeholder
+						 {
+						NumberOfNoMoreAccepted += 1
+						}
+				}
+				if maxAcceptedProposal!=0{
+					value = maxAcceptedProposal.acceptedValue
+				}else{
+					value = inputValue
+				}
+				if len(Majorityqueue) == NumberOfNoMoreAccepted {
+					state.PaxosNodeState.ProposerVolatileState.Prepared = true
+				}
+				break
+			}
+
 		}
-		if maxAcceptedProposal!=0{
-			value = maxAcceptedProposal.acceptedValue
-		}else{
-			value = inputValue
-		}
-		if len(Majorityqueue) == NumberOfNoMoreAccepted {
-			state.PaxosNodeState.ProposerVolatileState.Prepared = true
-		}
+		//placeholder
 
 	}
-	BroadcastPaxosMessage([accept])
-	NewAcceptPaxosMessage := <-
+
 	
+
 
 }
